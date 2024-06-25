2031715076028f65ff330671a29b255d900e27e0
Muskan Gupta <muskan_gupt@yahoo.com>
2024-06-24 18:07:12 -0700

    Exercise updates

--- context-api/src/ThemeContext.js
+++ context-api/src/ThemeContext.js
@@ -1,30 +1,21 @@
 import { createContext, useContext, useState } from "react";
 
-
 const ThemeContext = createContext(undefined);
 
-
-
 export const ThemeProvider = ({ children }) => {
-    const theme = useState({
-        theme: "dark",
-        toggleTheme: function () {
-            if (theme == "dark") {
-                theme = "light";
-            }
-            else {
-                theme = "dark";
-            }
-            console.log('Toggles theme'+theme);
-        }
-    })
-    return <ThemeContext.Provider value = {{theme}}></ThemeContext.Provider>
-};
+    const [theme, setTheme] = useState("light");
+  
+    return (
+      <ThemeContext.Provider
+        value={{
+          theme,
+          toggleTheme: () => setTheme(theme === "light" ? "dark" : "light"),
+        }}
+      >
+        {children}
+      </ThemeContext.Provider>
+    );
+  };
+  
 
 export const useTheme = () => useContext(ThemeContext);
-// export const useTheme = () => ({ theme: "light" });
-
-
-
-
-
